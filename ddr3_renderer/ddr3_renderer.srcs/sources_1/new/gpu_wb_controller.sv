7d74b3d4677feefbf17138208e3eeef8d873b3eb
Aniketh Tarikonda <aniketh8@illinois.edu>
2025-11-23 17:43:34 -0600

    bump gpu_wb_controller

--- ddr3_renderer/ddr3_renderer.srcs/sources_1/new/gpu_wb_controller.sv
+++ ddr3_renderer/ddr3_renderer.srcs/sources_1/new/gpu_wb_controller.sv
@@ -24,9 +24,13 @@ module gpu_wb_controller(
   input                   clk,
   input                   stall,
   input                   rst,
+// connection to rasterizer
   input logic [15:0]      din,
   input logic [26:0]      din_addr,
   input logic             din_valid,
+  output logic            ready,
+// connection to DDR3 arbiter
+  input logic             mem_wrdy,
   output logic [26:0]     dout_burst_addr,
   output logic [7:0]      dout_wrdm,
   output logic [127:0]    dout_burst_128,
@@ -34,64 +38,112 @@ module gpu_wb_controller(
 );
 
 typedef enum {
-  StIdle, StRead0, StRead1, StUpdate, StWriteback
+  StIdle, StRead0, StRead1, StWriteback, StFlush
 } wb_controller_state_e;
 
 wb_controller_state_e wb_controller_state_d, wb_controller_state_q;
 
-logic [127:0] wb_buffer;
-logic [26:0] din_addr_latched, buffer_addr;
+logic [127:0] wb_buffer_d, wb_buffer_q;
+logic [7:0] din_wrdm_q, din_wrdm_d;
+logic [23:0] din_tag_q, din_tag_d; // keep track of previous tags for comparison
+
+// address current value
+logic [2:0] curr_din_idx, din_idx_latched;
+logic [23:0] curr_din_tag, din_tag_latched;
 logic [15:0] din_latched;
-logic [2:0] curr_avail_q, curr_avail_d; // pointer to next available spot in wb_buffer
+
+assign curr_din_idx = din_addr[2:0]; // where in the 128-bit buffer is the value
+assign curr_din_tag = din_addr[26:3]; // where in ddr3 is this chunk located
 
 always_ff @(posedge clk) begin
   if (rst) begin
     wb_controller_state_q <= StIdle;
-    curr_avail_q <= 3'b0;
-    wb_buffer <= 128'b0;
-    din_addr_latched <= 27'b0;
+    wb_buffer_q <= 128'b0;
+    din_wrdm_q <= 8'b0;
+    din_tag_q <= 3'b0;
+
+    // latched values
+    din_idx_latched <= 3'b0;
+    din_tag_latched <= 24'b0;
     din_latched <= 16'b0;
+
   end else begin
     if (!stall) begin
       wb_controller_state_q <= wb_controller_state_d;
-      curr_avail_q <= curr_avail_d;
+      din_tag_q <= din_tag_d;
+      wb_buffer_q <= wb_buffer_d;
+      din_wrdm_q <= din_wrdm_d;
     end
 
-    // latch appropriate values
     if (din_valid) begin
-      din_latched = din;
-      din_addr_latched = din_addr;
+      din_idx_latched <= curr_din_idx;
+      din_tag_latched <= curr_din_tag;
+      din_latched <= din;
     end
   end
 end
 
 always_comb begin
   wb_controller_state_d = wb_controller_state_q;
+  wb_buffer_d = wb_buffer_q;
+  din_wrdm_d = din_wrdm_q;
+  din_tag_d = din_tag_q;
+
   dout_burst_valid = 1'b0;
-  dout_burst_128 = wb_buffer;
-  curr_avail_d = curr_avail_q;
+  dout_burst_128 = wb_buffer_q;
+  dout_wrdm = din_wrdm_q;
+  dout_burst_addr = {din_tag_q, 3'b0}; // reconstruct to 27 bits
+
+  ready = 1'b0;
 
   unique case (wb_controller_state_q)
     StIdle: begin
-      if (din_valid)
-        wb_controller_state_d = StRead0;
-      else
+      // this is only executed on resets & initially, when there is no previous tags/values to compare
+      if (din_valid) begin
+        wb_controller_state_d = StRead1;
+        wb_buffer_d[curr_din_idx*16 +: 16] = din; // part select
+        din_tag_d = curr_din_tag;
+        din_wrdm_d[curr_din_idx] = 1'b1;
+      end else begin
         wb_controller_state_d = StIdle;
+      end
+
+      ready = 1'b1;
     end
     StRead0: begin
-      // temporary state for latched values to settle
-      wb_controller_state_d = StRead1;
-    end
-    StRead1: begin
-      if (curr_avail_q == 3'b0) begin
-
+      // this is a duplicate state to avoid timing issues with din_valid, as well as provide a clock cycle for latches to update
+      if (din_valid) begin
+        wb_controller_state_d = StRead1;
+      end else begin
+        wb_controller_state_d = StRead0;
       end
     end
-    StUpdate: begin
+    StRead1: begin
+      if (din_tag_q == din_tag_latched) begin
+        wb_buffer_d[din_idx_latched*16 +: 16] = din_latched;
+        din_wrdm_d[din_idx_latched] = 1'b1;
+        ready = 1'b1;
+        wb_controller_state_d = StRead0;
+      end else begin
+        wb_controller_state_d = StWriteback;
+      end
     end
     StWriteback: begin
-      dout_burst_valid = 1'b1;
-      wb_controller_state_d = StIdle;
+      if (mem_wrdy) begin
+        dout_burst_valid = 1'b1;
+        din_wrdm_d = ~din_wrdm_d; // ddr3 expexts 0 to enable writes, and 1 to disable
+        wb_controller_state_d = StFlush;
+      end else
+        wb_controller_state_d = StWriteback;
+    end
+    StFlush: begin
+      wb_controller_state_d = StRead0;
+
+      // when we get this point, there is still some latched value waiting to be written
+      wb_buffer_d = {112'b0, din_latched} << (din_idx_latched*16); // shifts latched din into right place
+      din_wrdm_d = 8'b00000001 << (din_idx_latched);
+      din_tag_d = din_tag_latched;
+      ready = 1'b1;
     end
   endcase
 end
